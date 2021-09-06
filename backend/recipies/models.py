353ccc3859d10b299fc72fc2b4fc8e410938e3a7
VladMikheenko <mkhn.email@gmail.com>
2021-09-06 15:05:19 -0700

    models.py was initialized. The following models were added: Recipe, Tag, Ingredient, RecipeTag, RecipeIngredient. The last two are intermediate tables.

--- backend/recipies/models.py
+++ backend/recipies/models.py
@@ -1,3 +1,87 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# todo
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipies'
+        verbose_name='Author of a recipe'
+    )
+
+    name = models.CharField(max_length=200, verbose_name='Name of a recipe')
+    image = models.ImageField()
+    text = models.TextField(verbose_name='Description of a recipe')
+
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='RecipeIngredient'
+        verbose_name='Ingredients of a recipe'
+    )
+
+    tags = models.ManyToManyField(
+        Tag,
+        through='RecipeTag',
+        verbose_name='Tags of a recipe'
+    )
+
+    cooking_time = models.IntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(44640)
+        ]
+        verbose_name='Cooking time in minutes'
+    )
+
+    creation_date = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Date of creation'
+    )
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Name of a tag')
+    colour = models.CharField(max_length=64, verbose_name='Colour of a tag')
+    slug = models.SlugField(max_length=64, verbose_name='Slug of a tag')
+
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Name of an ingredient'
+    )
+
+    measurement_unit = models.CharField(max_length=32)
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE
+    )
+
+    ingredient = models.ForeigKey(
+        Ingredient,
+        on_delete=models.CASCADE
+    )
+
+    amount = models.IntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(44640)
+        ],
+        verbose_name='Amount of ingredients'
+    )
+
+
+class RecipeTag(models.Model):
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE
+    )
+
+    tag = models.ForeignKey(
+        Tag,
+        on_delete=models.CASCADE
+    )
